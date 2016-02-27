a4cd8e166dadc65f09acab0fe6a3c2d530eb329a
Ryan Wilson <rwilson@oberlin.edu>
2016-02-27 11:19:24 -0500

    Added Laser for enemies

---
+++ Laser.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Laser : Projectile {
+
+	// Use this for initialization
+	void Start () {
+		base.init (true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		base.Update ();
+	}
+}
