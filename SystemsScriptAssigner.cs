ccf5da1668bead2e5cf2e24a0e77f05917ea69a1
Ryan Wilson <rwilson@oberlin.edu>
2016-02-28 12:46:49 -0500

    Script for "Captain"
    Assigns all scripts to all objects in “Captain” prefab

---
+++ SystemsScriptAssigner.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SystemsScriptAssigner : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GameObject[] systems = new GameObject[3];
+		GameObject lasers = GameObject.FindGameObjectsWithTag ("Laser");
+		GameObject shields = GameObject.FindGameObjectsWithTag ("Shield");
+		GameObject engines = GameObject.FindGameObjectsWithTag ("Engine");
+		systems [0] = lasers;
+		systems [1] = shields;
+		systems [2] = engines;
+		foreach (GameObject i in systems) {
+			i.transform.FindChild ("Button").gameObject.AddComponent<ButtonClicker> ();
+			i.transform.FindChild ("Health").gameObject.AddComponent<ComponentHealth> ();
+		}
+	}
+}
