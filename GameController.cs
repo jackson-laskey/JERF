0d24204d64b41f59a4474c35f22fcaa86d40aad4
jackson-laskey <jackson.laskey@gmail.com>
2016-02-28 12:32:15 -0500

    Add GameController

---
+++ GameController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+	protected EnemyManager eMan;
+	protected Ship ship;
+	protected 
+
+	void Start () {
+		
+	}
+		
+	void ParseInstruction () {
+
+	}
+}
+
