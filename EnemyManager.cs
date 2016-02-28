74da9f5cab2ac07941f9b3676573cadbaf1befbc
ffirmo <ffirmo@oberlin.edu>
2016-02-28 12:32:18 -0500

    Enemy Manager
    Made the enemy manager script

---
+++ EnemyManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyManager : MonoBehaviour {
+
+	void SpawnEnemy(string type,float x, float y){
+		GameObject enemyObject = new GameObject();
+		ParentEnemy enemy;
+		if (type == "asteroid") {
+			enemy = enemyObject.AddComponent<Asteroid> ();
+			enemy.transform.position = new Vector3(x,y,0);
+			enemy.init (this);
+		}
+		if (type == "cannonenemy") {
+			enemy = enemyObject.AddComponent<CannonEnemy> ();
+			enemy.transform.position = new Vector3 (x, y, 0);
+			enemy.init (this);
+		}
+
+	}
+}
