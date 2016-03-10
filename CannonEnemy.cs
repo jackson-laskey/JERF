7a707e68b560eafa901f3ecbdf9edf30938280b1
Evan Lowy <elowy@oberlin.edu>
2016-03-10 15:43:40 -0500

    Code for added cannon enemy
    Full build will be up soon

--- CannonEnemy.cs
+++ CannonEnemy.cs
@@ -3,12 +3,18 @@ using System.Collections;
 
 public class CannonEnemy : ParentEnemy {
 
-	private CannonEnemyModel model;
+	//private CannonModel model;
 	private float stopPosition;
 	private string direction;
 	private string firingSide;
 	public AudioClip LaserSound;
 	private float cd;
+	private GameObject cannon;
+	private GameObject iCannon; //The cannon color. Ryan, this is were you can change the color for the boss
+	private Sprite[] cSprites; //Outline: 15, 16  Color: 0, 1
+	int oSprite = 15;
+	int iSprite = 0;
+	private float dmgCount = .3f;
 
 
 	private float sizex = .65f;
@@ -17,50 +23,86 @@ public class CannonEnemy : ParentEnemy {
 	public void init(EnemyManager owner) {
 		hp = 5;
 		fireRate = .42f;
-		speed = 2;
+		speed = -2;
 		transform.localScale = new Vector3 (sizex, sizey, 1);
 		col = gameObject.AddComponent<BoxCollider2D> ();
 		body = gameObject.AddComponent<Rigidbody2D> ();
+		SpriteRenderer rend = gameObject.AddComponent<SpriteRenderer> ();
+		this.GetComponent<Renderer> ().sortingOrder = 1;
+		animator = gameObject.AddComponent<Animator> ();
+		animator.runtimeAnimatorController = Resources.Load<RuntimeAnimatorController> ("Animation/CE_Animation_Controller");
+		animator.SetFloat ("MoveRate", .8f);
+		animator.SetBool ("Moving", true);
+		animator.SetBool ("Damaged", false);
 		body.isKinematic = true;
-		transform.eulerAngles = new Vector3(0,0,180);
+		transform.localScale = new Vector3 (1.5f, 1.5f, 0);
 		this.owner = owner;
 		LaserSound = Resources.Load ("Sounds/laser") as AudioClip;
 		gameObject.name = "CannonEnemy";
 		direction = "D";
 		firingSide = "L";
 		stopPosition = Random.Range (4.5f, -1f);
-		var modelObject = GameObject.CreatePrimitive (PrimitiveType.Quad);
-		model = modelObject.AddComponent<CannonEnemyModel>();	
-		model.init(this);
+		cannon = new GameObject ();
+		cannon.transform.parent = this.transform;
+		cannon.transform.localPosition = new Vector3 (0, -.36f, 0);
+		cannon.transform.localScale = new Vector2 (1, 1);
+		cannon.AddComponent<SpriteRenderer> ();
+		cSprites = Resources.LoadAll<Sprite> ("Textures/Cannon_Enemy_Sprite_Sheet");
+		cannon.GetComponent<SpriteRenderer> ().sprite = cSprites [oSprite];
+		cannon.name = "CannonOutline";
+		iCannon = new GameObject ();
+		iCannon.transform.parent = this.transform;
+		iCannon.transform.localPosition = new Vector3 (0, -.36f, 0);
+		iCannon.transform.localScale = new Vector2 (1, 1);
+		iCannon.AddComponent<SpriteRenderer> ();
+		iCannon.GetComponent<SpriteRenderer> ().sprite = cSprites[iSprite];
+		iCannon.name = "CannonColor";
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
 		if (hp <= 0) {
-			Destroy (this.gameObject);
+			animator.SetBool ("Damaged", false);
+			speed = 0;
+			Die ();
 		}
+		if (animator.GetBool ("Damaged")) {
+			dmgCount = dmgCount - Time.deltaTime;
+		}
+		if (dmgCount <= 0) {
+			animator.SetBool ("Damaged", false);
+			dmgCount = .3f;
+		}
+
 		if (transform.position.y < stopPosition) {
 			direction = "L";	
 			transform.position = new Vector3(transform.position.x,stopPosition,0);
+
 		}
 		if (transform.position.x <= -6) {
 			direction = "R";
 		}
-		if (transform.position.x >= 0) {
+		if (transform.position.x >= -1) {
 			direction = "L";
 		}
 		Move ();
 
 
 		if (transform.position.y == stopPosition) {
+			
+			cannon.GetComponent<SpriteRenderer> ().sprite = cSprites [oSprite];
+			iCannon.GetComponent<SpriteRenderer> ().sprite = cSprites[iSprite];
+			animator.SetBool ("Moving", false);
 			if (cd <= 0) {
 				if (firingSide == "L") {
-					Fire ((transform.position.x - .25f), transform.position.y);
+					Fire ((transform.position.x - .45f), (transform.position.y -.36f));
 					firingSide = "R";
 					cd = fireRate;
 				}
 				else if (firingSide == "R") {
-					Fire ((transform.position.x + .25f), transform.position.y);
+					Fire ((transform.position.x + .45f), (transform.position.y-.36f));
 					firingSide = "L";
 					cd = fireRate;
 				}
@@ -84,7 +126,13 @@ public class CannonEnemy : ParentEnemy {
 	//I made this take x and y because I was thinking about it and different enemies will need to fire from different parts of their models
 	protected void Fire(float x, float y){ 	
 		AudioSource.PlayClipAtPoint(LaserSound, transform.position);
-			
+		if (oSprite == 15 && iSprite == 0) {
+			oSprite = 16;
+			iSprite = 1;
+		} else {
+			oSprite = 15;
+			iSprite = 0;
+		}
 		GameObject shot = new GameObject();
 		shot.transform.parent = transform.parent;
 		shot.transform.position = new Vector3 (0, 0);
@@ -96,9 +144,18 @@ public class CannonEnemy : ParentEnemy {
     void OnTriggerEnter2D(Collider2D other){
 		if (other.name == "PlayerLaser") {
 			hp--;
+			animator.SetBool ("Damaged", true);
 		}
 		if (other.name == "SuperPlayerLaser") {
 			hp--;
+			animator.SetBool ("Damaged", true);
 		}
+	}
+
+	void Die(){
+		Destroy (cannon.gameObject);
+		Destroy (iCannon.gameObject);
+		animator.SetTrigger ("Die");
+		Destroy (this.gameObject, 1.1f);
 	}
 }
