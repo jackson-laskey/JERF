ead91ac722e660b85633772e5d5087836122724b
Evan Lowy <elowy@oberlin.edu>
2016-03-10 12:10:11 -0500

    added Instructions

--- ScreenController.cs
+++ ScreenController.cs
@@ -19,9 +19,9 @@ public class ScreenController : MonoBehaviour {
 		if (GUI.Button(new Rect(Screen.width/2 - 40, Screen.height/2 -40, 100,50), "Start Game")){
 			Application.LoadLevel ("Mechanic Study");
 		}
-		//if (GUI.Button (new Rect (Screen.width/2 - 40, Screen.height/2+15, 100, 50), "Instructions")) {
+		if (GUI.Button (new Rect (Screen.width/2 - 40, Screen.height/2+15, 100, 50), "Instructions")) {
 
-		//}
+		}
 		if (GUI.Button (new Rect (Screen.width/2 -40, Screen.height/2 + 70, 100, 25), "Quit")) {
 			Application.Quit();
 		}
