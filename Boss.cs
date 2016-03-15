d4b3c74d730e69f52a5b6998857f362389bc159c
jackson-laskey <jackson.laskey@gmail.com>
2016-03-15 10:16:12 -0400

    Add sounds to boss

--- Boss.cs
+++ Boss.cs
@@ -5,6 +5,7 @@ public class Boss : CannonEnemy {
 
 	public AudioClip BeamSound;
 	public AudioClip explosion;
+	public AudioClip bossEntrance;
 	public AudioSource audio;
 	public bool dead = false;
 
@@ -45,7 +46,9 @@ public class Boss : CannonEnemy {
 		rMat.color = laserColor;
 
 		BeamSound = Resources.Load ("Sounds/beamSound") as AudioClip;
-		explosion = Resources.Load ("Sounds/explosion") as AudioClip;
+		explosion = Resources.Load ("Sounds/bossDeath") as AudioClip;
+		bossEntrance = Resources.Load ("Sounds/bossEntrance") as AudioClip;
+		AudioSource.PlayClipAtPoint (bossEntrance, transform.position);
 
 		audio = gameObject.AddComponent<AudioSource> ();
 		audio.loop = true;
